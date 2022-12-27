9a27723835f6318a5435a07abef8a7c6b7940218
OvercomingJunk <overcomingjunk@yandex.ru>
2022-12-27 21:14:57 +0300

    Improved the query parsing logic

--- src/main/java/ru/job4j/pooh/Req.java
+++ src/main/java/ru/job4j/pooh/Req.java
@@ -6,6 +6,8 @@ package ru.job4j.pooh;
  * @since 27.12.2022
  */
 public class Req {
+    private static final String GET = "GET";
+    private static final String POST = "POST";
     private final String httpRequestType;
     private final String poohMode;
     private final String sourceName;
@@ -19,8 +21,17 @@ public class Req {
     }
 
     public static Req of(String content) {
-        /* TODO parse a content */
-        return new Req(null, null, null, null);
+        String[] partsOfContent = content.split(System.lineSeparator());
+        String[] mainContent = partsOfContent[0].split("/");
+        String httpRequestType = mainContent[0].trim();
+        String param = "";
+        if (GET.equals(httpRequestType) && mainContent.length == 5) {
+            param = mainContent[mainContent.length - 2].split(" ")[0];
+        }
+        if (POST.equals(httpRequestType)) {
+            param = partsOfContent[partsOfContent.length - 1];
+        }
+        return new Req(httpRequestType, mainContent[1], mainContent[2].split(" ")[0], param);
     }
 
     public String httpRequestType() {
